d6af7bf6c9f75e4317edea5fde5801cd05d18e91
Lance Parsons <lparsons@princeton.edu>
2018-08-02 16:55:38 -0400

    Add time parameter to cetus run script

--- run_snakemake_cetus.sh
+++ run_snakemake_cetus.sh
@@ -5,5 +5,5 @@
 # TODO Optional parameters to specify config and cluster files
 
 snakemake --cluster-config 'cluster_config.cetus.yml' \
-          --drmaa " --cpus-per-task={cluster.n} --mem={cluster.memory} --qos={cluster.qos}" \
+          --drmaa " --cpus-per-task={cluster.n} --mem={cluster.memory} --time={cluster.time} --qos={cluster.qos}" \
           --use-conda -w 60 -rp -j 1000 "$@"
